b691642d309d4a2e1c6134acca64b45ba9b6d954
bodhibrdr <julie.gumerman@gmail.com>
2019-09-13 11:01:54 -0600

    create a smurf in console.log

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,10 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { connect } from "react-redux";
 import { getSmurfs } from "../actions/index";
 
 
 const App = ({ getSmurfs, smurfs}) => {
+  const [newSmurf, setNewSmurf] = useState({name: "", age: "", height: "", id: ""})
+
+    const handleChange = event => {
+      setNewSmurf({...newSmurf, [event.target.name]: event.target.value});
+    }
+
+    const submitHandler = event => {
+      event.preventDefault();
+      console.log(newSmurf);
+    }
 
     useEffect(() => {
       getSmurfs();
@@ -16,9 +26,18 @@ const App = ({ getSmurfs, smurfs}) => {
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
-        
+        <div>
         {smurfs.map(smurf => <h3>{smurf.name}</h3>)}
+      </div>
+      <form>
+        <label>Add a new smurf!</label>
+        <input placeholder="name" name="name" onChange={event => handleChange(event) }/>
+        <input placeholder="age" name="age" onChange={event => handleChange(event)}/>
 
+        <input placeholder="height" name="height" onChange={event => handleChange(event)}/>
+        <input id="id" name="id" onChange={event => handleChange(event)}/>
+        <button type="submit" onClick={submitHandler}/>
+      </form>
       </div>
     );
 }
